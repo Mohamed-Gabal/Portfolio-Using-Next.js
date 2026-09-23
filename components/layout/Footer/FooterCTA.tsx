86ff7ad47ae64017adae2a3dd4ffd12016cc1305
Mohamed Gabal <abogabal672@gmail.com>
2026-09-23 18:52:04 +0300

    edit link start project

--- components/layout/Footer/FooterCTA.tsx
+++ components/layout/Footer/FooterCTA.tsx
@@ -13,12 +13,12 @@ const FooterCTA = () => {
         </div>
       </div>
 
-      <Link
+      <a
         href="#"
         className="rounded-lg bg-blue-600 px-5 py-2 text-sm font-medium text-white transition hover:bg-blue-700"
       >
         Start a Project
-      </Link>
+      </a>
     </div>
   );
 };
